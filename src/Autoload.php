2e1fb46bc225a3ab845ef67bc7e1a4d767faef40
JonPurvis <jonathan.purvis@visualsoft.co.uk>
2024-02-11 01:29:04 +0000

    lint

--- src/Autoload.php
+++ src/Autoload.php
@@ -4,9 +4,6 @@ declare(strict_types=1);
 
 namespace Pest\PluginName;
 
-use Pest\Plugin;
-use PHPUnit\Framework\TestCase;
-
 expect()->extend('toBeSaloonConnector', function () {
     return $this->toExtend('Saloon\Http\Connector');  // @phpstan-ignore-line
 });
--- tests/Fixtures/Arch/ToBeSaloonConnector/IsNotConnector.php
+++ tests/Fixtures/Arch/ToBeSaloonConnector/IsNotConnector.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Tests\Fixtures\Arch\ToBeSaloonConnector;
 
-use Saloon\Http\Connector;
-
 class IsNotConnector
 {
     public function resolveBaseUrl(): string
