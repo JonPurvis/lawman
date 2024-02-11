48a84266da11b6befbcfcb6d6fe962c75265beb9
JonPurvis <jonathan.purvis@visualsoft.co.uk>
2024-02-11 03:14:53 +0000

    change package name

--- src/Autoload.php
+++ src/Autoload.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Pest\PluginName;
+namespace JonPurvis\PestPluginSaloon;
 
 expect()->extend('toBeSaloonConnector', function () {
     return $this->toExtend('Saloon\Http\Connector');  // @phpstan-ignore-line
